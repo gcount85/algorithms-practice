6f34ca8efa51156e58a8c080a197312dc21a824d
gcount85 <100mgml@gmail.com>
2022-11-01 22:09:31 +0900

    하노이탑 약간 수정

--- bj-1914-하노이탑.py
+++ bj-1914-하노이탑.py
@@ -3,7 +3,7 @@ import sys
 
 def 하노이(n, a, b, c):  # 원판갯수, 시작위치, 보조, 끝위치
     if n == 1:
-        print(f"{a} {c}")
+        print(a, c)
     else:
         하노이(n-1, c, a, b)
         하노이(1, a, b, c)
