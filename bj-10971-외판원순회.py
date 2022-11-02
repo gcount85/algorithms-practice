211865177338ad06d3d79b8cc5eb2b8ccd4ec5ae
gcount85 <100mgml@gmail.com>
2022-11-02 18:54:33 +0900

    외판원 순회 메모리 초과

--- bj-10971-외판원순회.py
+++ bj-10971-외판원순회.py
@@ -17,46 +17,49 @@ N = 3
     [5, 0, 2],
     [0, 3, 0],
 ]
-# print(비용)
 
 도시목록 = range(N)
-도시목록순열 = []  # 마지막에 첫번째 수를 다시 추가해야 함! 
-# print(도시목록순열)
+도시방문순열 = list(permutations(도시목록))  # 마지막에 첫번째 수를 다시 추가해야 함! 
+# print(도시방문순열)
 
 # 방문할 수 없는 도시들의 인덱스 모음
-인덱스 = []
-for i, v in enumerate(비용):
-    print(i, v)
-    if v.count(0) > 1:
-        for j, k in enumerate(v):
-            if j != i and k == 0:   
-                인덱스.append((i, j))
-print(인덱스)
+# 리스트컴프리헨션
+인덱스 = [(i, j) for i, v in enumerate(비용) if v.count(0) > 1 for j, k in enumerate(v) if j != 1 and k == 0]
+
+# 그냥 for append
+# 인덱스 = []
+# for i, v in enumerate(비용):
+#     # print(i, v)
+#     if v.count(0) > 1:
+#         for j, k in enumerate(v):
+#             if j != i and k == 0:   
+#                 인덱스.append((i, j))
+# print(인덱스)
 
 # 방문할 수 없는 도시들의 인덱스를 참조하여 유효하지 않은 순열 제거
 for c in permutations(도시목록):       # 0, 2, 1
-    for i in 인덱스:         # 0, 1
-        for n in range(N):   # 0, 1, 2
+    for ii in 인덱스:         # 0, 1
+        for n in 도시목록:   # 0, 1, 2
             # print(i)
             if n == N-1:
-                if c[N-1]+c[0] != i:
-                    도시목록순열.append(c)
+                if ((c[N-1], c[0]) == ii) and (c in 도시방문순열):
+                    도시방문순열.remove(c)
             else:
-                if c[n:n+2] != i:
-                    도시목록순열.append(c)
-print(도시목록순열)
+                if (c[n:n+2] == ii) and (c in 도시방문순열):
+                    도시방문순열.remove(c)
+# print(도시방문순열)
 
 # 순열의 비용을 재귀적으로 구하는 함수~~ 
-# def 비용더하기(i, 순열):
-#     # (0, 3, 2, 1)
-#     if i == N-1:  #인덱스가 3 
-#         return 비용[순열[i]][순열[0]] 
-#     else:
-#         return 비용[순열[i]][순열[i+1]] + 비용더하기(i+1, 순열)
+def 비용더하기(i, 순열):
+    # (0, 3, 2, 1)
+    if i == N-1:  #인덱스가 3 
+        return 비용[순열[i]][순열[0]] 
+    else:
+        return 비용[순열[i]][순열[i+1]] + 비용더하기(i+1, 순열)
 
-# 비용합목록 = [비용더하기(0, i) for i in 도시목록순열]
+비용합목록 = [비용더하기(0, i) for i in 도시방문순열]
 
-# print(min(비용합목록))
+print(min(비용합목록))
 
 
 
---
+++ untitle.py
@@ -0,0 +1,16 @@
+
+ex = (0, 2, 1)
+no = (2, 1)
+N = 3
+
+print((ex[N-1], ex[0]))
+
+
+# for n in range(N):   # 0, 1, 2
+#     # print(i)
+#     if n == N-1:
+#         if ex[N-1]+ex[0] != no[:]:
+#     else:
+#         if ex[n:n+2] != no[:]:
+#             print(ex[n:n+2])
+
