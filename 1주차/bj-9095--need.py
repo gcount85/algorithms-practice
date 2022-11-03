05fe8c1e640bf6eb5557e42a175ae1cff41fc5d5
gcount85 <100mgml@gmail.com>
2022-11-03 13:51:47 +0900

    문제 주차별로 분류

---
+++ 1주차/bj-9095--need.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/9095
+# 1, 2, 3 더하기
+
+import sys
+from itertools import combinations_with_replacement
+
+# T = int(sys.stdin.readline())
+T = 3
+N = 4
+
+comb = 
+
+
