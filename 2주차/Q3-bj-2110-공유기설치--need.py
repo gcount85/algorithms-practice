b3f8b2876b798ed4c7340af0073a6d2fdaba5b52
gcount85 <100mgml@gmail.com>
2022-11-04 19:00:31 +0900

    두 용액 문제 추가

--- 2주차/Q3-bj-2110-공유기설치--need.py
+++ 2주차/Q3-bj-2110-공유기설치--need.py
@@ -1,9 +1,7 @@
 # https://www.acmicpc.net/problem/2110
 
-
 import sys
 import numpy as np
-from itertools import combinations
 
 집의개수N = 5
 공유기개수C = 3   #
@@ -11,10 +9,7 @@ from itertools import combinations
 
 집좌표.sort()
 
-# 공유기경우의수 = combinations(집좌표, 공유기개수C)
-
-
-def search(list, 공유기개수C, low, high):  # 인덱스로 안함당
+def search(list, low, high):  # 인덱스로 안함당
     global 집좌표
     low = list[0]
     high = list[-1]
@@ -30,3 +25,5 @@ def search(list, 공유기개수C, low, high):  # 인덱스로 안함당
             return find_H(M, H_range, low, mid-1)
         else:
             return find_H(M, H_range, mid+1, high)
+
+search(집좌표, 1, )
