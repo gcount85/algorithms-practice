953fd8755c2ae1692aae322d67657653bf707300
gcount85 <100mgml@gmail.com>
2022-11-08 20:57:17 +0900

    가운데를 말해요 문제 해결

--- 2주차/Q25-bj-1655-가운데를말해요--heapq모듈.py
+++ 2주차/Q25-bj-1655-가운데를말해요--heapq모듈.py
@@ -1,30 +1,40 @@
 # https://www.acmicpc.net/problem/1655
 
-import sys
 import heapq
+import sys
 
+'''
+1. leftheap과 rightheap의 길이가 같다면(즉 두 heap에 들어있는 리스트 요소의 수가 같으면) 
+중간값의 기준이 되는 leftheap에 수를 넣는다. 
+반면, 길이가 같지 않다면 길이를 맞춰주기 위해 rightheap에 수를 넣는다.
+
+2. 만약에 leftheap의 루트가 rightheap의 루트보다 크면 
+leftheap의 루트와 rightheap의 루트를 바꿔준다.
+why? leftheap은 중간값을 기준으로 작은 수가 들어가는 곳이다.
+ 그런데 leftheap의 루트가 rightheap보다 크다면, 중간값보다 큰 수가 leftheap에 들어가있는 상황이기에 
+ leftheap의 루트와 rightheap의 루트를 바꿔준다.
+'''
 
 N = int(sys.stdin.readline())
-que = []
+smallerheap = []  # 최대힙
+biggerheap = []   # 최소힙 
 
 for _ in range(N):
     input = int(sys.stdin.readline())
-    heapq.heappush(que, input)
-    que_size = len(que)
-    if que_size == 1:
-        print(input)
+    if len(smallerheap) == len(biggerheap):    # len() 변수로 지정 안 하는게 나음(계속 변하니)
+        heapq.heappush(smallerheap, -input)
     else:
-        if que_size == 2:
-            pop_out = heapq.heappop(que)
-            print(pop_out)
-            heapq.heappush(que, pop_out)
-        else:
-            temp = []
-            while len(que) != (que_size//2)+1:
-                pop_out = heapq.heappop(que)
-                temp.append(pop_out)
-            pop_out = heapq.heappop(que)
-            print(pop_out)
-            temp.append(pop_out)
-            for i in temp:
-                heapq.heappush(que, i)
+        heapq.heappush(biggerheap, input)
+    if (len(biggerheap) != 0) and (-smallerheap[0] > biggerheap[0]):
+        # biggerheap[0], smallerheap[0] = -smallerheap[0], -biggerheap[0]  # 이렇게 하면 틀림
+        min = heapq.heappop(biggerheap)
+        max = -heapq.heappop(smallerheap)
+        heapq.heappush(smallerheap, -min)
+        heapq.heappush(biggerheap, max)
+    print(-smallerheap[0])
+
+
+
+
+
+
