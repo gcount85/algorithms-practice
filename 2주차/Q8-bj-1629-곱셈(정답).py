b02f2b2b3f105187ceb7b7b65650fd597eecf9d1
gcount85 <100mgml@gmail.com>
2022-11-09 20:36:07 +0900

    약간의 수정

--- 2주차/Q8-bj-1629-곱셈(정답).py
+++ 2주차/Q8-bj-1629-곱셈(정답).py
@@ -1,16 +1,16 @@
 # https://velog.io/@grace0st/%EA%B3%B1%EC%85%88-%EB%B0%B1%EC%A4%80-1629%EB%B2%88-%ED%8C%8C%EC%9D%B4%EC%8D%AC
 
 import sys
-a,b,c = map(int,sys.stdin.readline().split())
+a, b, c = map(int,sys.stdin.readline().split())
 
-def multi (a,n):
+def multi(a, n):
   if n == 1:
-      return a%c
+      return a % c
   else:
-      tmp = multi(a,n//2)
-      if n %2 ==0:
+      tmp = multi(a, n//2)
+      if n % 2 ==0:
           return (tmp * tmp) % c
       else:
-          return (tmp  * tmp *a) %c
+          return (tmp * tmp * a) % c
           
-print(multi(a,b))
+print(multi(a, b))
