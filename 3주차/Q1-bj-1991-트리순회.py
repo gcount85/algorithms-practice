3c7e55abfe4ee2be5dd4fd972a76d1884c5ad424
gcount85 <100mgml@gmail.com>
2022-11-11 22:35:07 +0900

    트리 순회 이진트리 코드 추가2

--- 3주차/Q1-bj-1991-트리순회.py
+++ 3주차/Q1-bj-1991-트리순회.py
@@ -1,14 +1,13 @@
-import sys
 
 from __future__ import annotations
 from typing import Any, Type
+import sys
 
 class Node:
     '''이진 검색 트리의 노드'''
-    def __init__(self, key: Any, value: Any, left: Node = None, right: Node = None):
+    def __init__(self, key: Any, left: Node = None, right: Node = None):
         '''생성자(constructor)'''
         self.key = key      # 키
-        self.value = value  # 값
         self.left = left    # 왼쪽 포인터
         self.right = right  # 오른쪽 포인터
 
@@ -26,37 +25,46 @@ class BinaryTree:
             if p is None:
                 return None
             if key == p.key:
-                return p.value
+                return p.key
 
-    def add(self, key: Any, value: Any) -> bool:
-        '''키가 key이고 값이 value인 노드를 삽입'''
+    def add(self, key: Any, index: int) -> bool:
+        '''키가 key인 노드를 삽입'''
 
-
-        def add_left_node(node: Node, key: Any, value: Any) -> None:
-            '''node를 루트로 하는 서브트리의 왼쪽 자식에 키가 key이고 값이 value인 노드를 삽입'''
+        def add_left_node(node: Node, key: Any) -> None:
+            '''node를 루트로 하는 서브트리의 왼쪽 자식에 키가 key인 노드 삽입'''
             if node.left is None:
-                node.left = Node(key, value, None, None)
+                node.left = Node(key, None, None)
             else:
-                add_left_node(node.left, key, value)
+                add_left_node(node.left, key)
 
-        def add_right_node(node: Node, key: Any, value: Any) -> None:
-            '''node를 루트로 하는 서브트리에 오른쪽 자식에 키가 key이고 값이 value인 노드를 삽입'''
+        def add_right_node(node: Node, key: Any) -> None:
+            '''node를 루트로 하는 서브트리에 오른쪽 자식에 키가 key인 노드 삽입'''
             if node.right is None:
-                node.right = Node(key, value, None, None)
+                node.right = Node(key, None, None)
             else:
-                add_right_node(node.right, key, value)
+                add_right_node(node.right, key)
             return True
 
         if self.root is None:
-            self.root = Node(key, value, None, None)
+            self.root = Node(key, None, None)
             return True
-        elif #왼쪽이면
-            return add_left_node(self.root, key, value)
-        elif #오른쪽이면
-            return add_right_node(self.root, key, value)
+        elif index == 1:
+            return add_left_node(self.root, key)
+        elif index == 2:
+            return add_right_node(self.root, key)
 
 
 
 N = int(sys.stdin.readline())
 for _ in range(N):
-    input = int(sys.stdin.readline())
+    node = sys.stdin.readline().split()  # [A, B, C]
+    value = node[0]
+    if node[1] != '.':
+        left = node[1]
+        BinaryTree.add(node[0], 1)
+    if node[2] != '.':
+        right = node[2]
+        BinaryTree.add(node[0], 2)
+    
+
+
---
+++ 구현코드/이진트리.py
@@ -0,0 +1,59 @@
+import sys
+
+from __future__ import annotations
+from typing import Any, Type
+
+class Node:
+    '''이진 검색 트리의 노드'''
+    def __init__(self, key: Any, value: Any, left: Node = None, right: Node = None):
+        '''생성자(constructor)'''
+        self.key = key      # 키
+        self.value = value  # 값
+        self.left = left    # 왼쪽 포인터
+        self.right = right  # 오른쪽 포인터
+
+class BinaryTree:
+    '''이진 트리'''
+
+    def __init__(self):
+        '''초기화'''
+        self.root = None
+    
+    def search(self, key: Any) -> Any:
+        '''키가 key인 노드를 검색 '''
+        p = self.root
+        while True:
+            if p is None:
+                return None
+            if key == p.key:
+                return p.value
+
+    def add(self, key: Any, value: Any, index: int) -> bool:
+        '''키가 key이고 값이 value인 노드를 삽입'''
+
+
+        def add_left_node(node: Node, key: Any, value: Any) -> None:
+            '''node를 루트로 하는 서브트리의 왼쪽 자식에 키가 key이고 값이 value인 노드를 삽입'''
+            if node.left is None:
+                node.left = Node(key, value, None, None)
+            else:
+                add_left_node(node.left, key, value)
+
+        def add_right_node(node: Node, key: Any, value: Any) -> None:
+            '''node를 루트로 하는 서브트리에 오른쪽 자식에 키가 key이고 값이 value인 노드를 삽입'''
+            if node.right is None:
+                node.right = Node(key, value, None, None)
+            else:
+                add_right_node(node.right, key, value)
+            return True
+
+        if self.root is None:
+            self.root = Node(key, value, None, None)
+            return True
+        # elif index == 1:
+        #     return add_left_node(self.root, key, value)
+        # elif index == 2:
+        #     return add_right_node(self.root, key, value)
+
+
+
