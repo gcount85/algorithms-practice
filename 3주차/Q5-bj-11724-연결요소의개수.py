d113a76a9f4e2ab44b262ee5d0cd2f57ad5730c3
gcount85 <100mgml@gmail.com>
2022-11-13 22:12:05 +0900

    연결 요소의 개수 44% 성공

---
+++ 3주차/Q5-bj-11724-연결요소의개수.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10**9)
+
+N, M = map(int, sys.stdin.readline().strip().split())
+edges = [[] for _ in range(N+1)]
+for _ in range(M):
+    src, dst = map(int, sys.stdin.readline().strip().split())
+    edges[src].append(dst)  
+print(edges)
+
+count = 0
+parent = {}
+
+def dfs_visit(s, edges):
+    for v in edges[s]:
+        if v not in parent:     
+            parent[v] = s
+            dfs_visit(v, edges)
+            
+
+# dfs 함수는 단절된 그래프, 강하게 연결된 그래프가 아닌 경우에 시작 노드를 바꿔 모든 그래프를 탐색하기 위함
+def dfs(N, edges):
+    global count
+    for s in range(1,N+1):
+        if s not in parent:
+            parent[s] = None
+            count += 1
+            dfs_visit(s, edges)
+
+dfs(N, edges)
+# for i in parent.values():
+#     if i == None:
+#         count += 1
+print(parent)
