272af263601f8edbfd0af1bbcdd1203e803b18eb
gcount85 <100mgml@gmail.com>
2022-11-13 15:10:18 +0900

    최소 스패닝 트리 인덱스 체크방식 -- 인덱스 에러

--- 3주차/Q3-bj-1197-최소스패닝트리.py
+++ 3주차/Q3-bj-1197-최소스패닝트리.py
@@ -1,12 +1,16 @@
 import sys
 
 V, E = map(int, sys.stdin.readline().split())
-W = [list(map(int, sys.stdin.readline().split())) for _ in range(E)]
+edges = [[] for _ in range(E)]
+for _ in range(E):
+    src, dst, w = map(int, sys.stdin.readline().split()) 
+    edges[src].append((dst, w))   
+
+# print(edges)
 
 '''
 노드는 1번부터 V번까지 존재 
 '''
-
 # V = 3  # 노드개수 
 # E = 3  # 엣지갯수
 # W = [[1, 2, 1],
@@ -17,31 +21,29 @@ W = [list(map(int, sys.stdin.readline().split())) for _ in range(E)]
 # W = [[1, 2, 1],
 #      [2, 1, 1],
 #      [2, 3, 3],
-#      [3, 1, 3]]
+#      [3, 1, 0]]
 
 # 가중치의 합 
-uncovered = list(range(2,V+1))
+covered = [1] + [[] for _ in range(V-1)]
+# print(covered)
+chk = set()
 total = 0
 
-while len(uncovered) != 0:
+while len(chk) != V:
     tmp = 2147483648
-    for v in W:
-        if v[0] != -1:
-            if ((v[0] not in uncovered) and (v[1] in uncovered)) or ((v[1] not in uncovered) and (v[0] in uncovered)):
-                if (tmp >= v[2]):
-                    tmp = v[2]
-                    mini_edge = v  # 최소가중치를 가진 엣지
-    total += mini_edge[2]
-    src = mini_edge[0]
-    dst = mini_edge[1]
-    for j in W:
-        if (j[0] == src and j[1] == dst) or (j[0] == dst and j[1] == src):
-            j[0] = -1
-    if dst in uncovered:
-        uncovered.remove(dst)
-    if src in uncovered:
-        uncovered.remove(src)
-
+    for i, v in enumerate(edges):      # i = 0,1,2   #v = [], [(2, 1), (3, 3)], [(3, 2)]
+        if (i > 0) and (covered[i-1] != [] and v != []):  # 해당 노드가 커버되어있고, 그 노드에서 다른 노드로 가는 엣지들이 존재할 때
+            for j in v:  # [(2, 1), (3, 3)]
+                if (covered[j[0]-1] == []) and (tmp > j[1]):
+                    tmp = j[1]
+                    mini_edge = j  # 최소가중치를 가진 엣지의 (dst, 가중치)
+                    src = i
+    total += mini_edge[1]
+    covered[src-1] = src    
+    dst = mini_edge[0]
+    covered[dst-1] = dst 
+    chk.add(src)
+    chk.add(dst)
 # 최소 스패닝 트리의 가중치를 출력
 print(total)
 
--- 연습장.py
+++ 연습장.py
@@ -1,36 +1,10 @@
-# https://www.acmicpc.net/problem/1920
+a = [1, 2, 3]
+b = None
 
-import sys
 
-def bin_search(arr, src):
-    pl = 0
-    pr = len(arr) - 1
-
-    while True:
-        pc = (pl + pr) // 2
-
-        if arr[pc] == src:
-            return "1"
-        elif arr[pc] < src:
-            pl = pc + 1
-        else:
-            pr = pc - 1
-
-        if pl > pr:
-            break
-    return "0"
-
-print_list = ""
-
-count = int(sys.stdin.readline())
-num = list(map(int, sys.stdin.readline().split()))
-
-src_count = int(sys.stdin.readline())
-src_num = list(map(int, sys.stdin.readline().split()))
-
-num.sort()
-for i in src_num:
-    print_list += bin_search(num,i)
-
-for i in range(len(print_list)):
-    print(print_list[i])
+if a == []:
+    print("a는 []이다")
+if a == None:
+    print("a는 []이면서 None이다")
+if b == []:
+    print("b는 none이면서 []이다")
