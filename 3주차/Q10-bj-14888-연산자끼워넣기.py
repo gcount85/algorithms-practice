d5b75b035a987773c295e61d30d7ab65f07ae976
100mgml@gmail.com <100mgml@gmail.com>
2022-11-14 16:16:05 +0900

    연산자 끼워넣기 TC 성공

--- 3주차/Q10-bj-14888-연산자끼워넣기.py
+++ 3주차/Q10-bj-14888-연산자끼워넣기.py
@@ -6,8 +6,8 @@ sys.setrecursionlimit(10**9)
 
 N = int(sys.stdin.readline().strip())
 A = list(map(int, sys.stdin.readline().strip().split()))
-operator = list(map(int, sys.stdin.readline().strip().split()))  # + - X ％ → 2 1 1 1 
-연산자 = ['+', '-', '*', '/']
+operator = list(map(int, sys.stdin.readline().strip().split()))  # + - X / → 2 1 1 1 
+연산자 = ['+', '-', '*', '//']
 엣지갯수 = sum(operator)
 edges = [[] for _ in range(엣지갯수)]
 
@@ -22,33 +22,41 @@ for j, v in enumerate(operator):     #2, 1, 1, 1
 최소 = 1000000000
 최대 = -1000000000
 
-def dfs_visit(idx: int, i: tuple, i_ele: str):        # i = ('+', '%', '*', '-', '+')
+def dfs_visit(idx: int, i: tuple):        # i = ('+', '/', '*', '-', '+')
     global tmp_max, tmp_min, A
-    if idx == N:
+    if idx == N-1:
         return
-    elif idx == 0:
-    # print("s는", s, "계산값은", eval(i_ele.join([str(s), str(s+1)])))
-        tmp_min = eval(i_ele.join([str(A[idx]), str(A[idx+1])]))
-        tmp_max = eval(i_ele.join([str(A[idx]), str(A[idx+1])]))
-        dfs_visit(idx+1, i, i[idx+2])
     else:
-        tmp_min = eval(i_ele.join([str(tmp_min), str(A[idx+1])]))
-        tmp_max = eval(i_ele.join([str(tmp_max), str(A[idx+1])]))
-        dfs_visit(idx+1, i, i[idx+1])
-
+        if idx == 0:
+            tmp_min = eval(i[idx].join([str(A[idx]), str(A[idx+1])]))
+            tmp_max = eval(i[idx].join([str(A[idx]), str(A[idx+1])]))
+        else:
+            if i[idx] == '//':
+                if tmp_max < 0:
+                    tmp_max = -eval(i[idx].join([str(-tmp_max), str(A[idx+1])]))
+                    tmp_min = eval(i[idx].join([str(tmp_min), str(A[idx+1])]))
+                if tmp_min < 0:
+                    tmp_min = -eval(i[idx].join([str(-tmp_min), str(A[idx+1])]))
+                    tmp_max = eval(i[idx].join([str(tmp_max), str(A[idx+1])]))
+                else:
+                    tmp_min = eval(i[idx].join([str(tmp_min), str(A[idx+1])]))
+                    tmp_max = eval(i[idx].join([str(tmp_max), str(A[idx+1])]))
+            else:
+                tmp_min = eval(i[idx].join([str(tmp_min), str(A[idx+1])]))
+                tmp_max = eval(i[idx].join([str(tmp_max), str(A[idx+1])]))
+        dfs_visit(idx+1, i)
 
 for i in 연산자순열:
     tmp_min = 0
     tmp_max = 0
     idx = 0
-    dfs_visit(idx, i, i[idx])
+    dfs_visit(idx, i)
     if tmp_min < 최소:
         최소 = tmp_min
     if tmp_max > 최대:
         최대 = tmp_max
 
-
-print(최소)
 print(최대)
+print(최소)
 
 
