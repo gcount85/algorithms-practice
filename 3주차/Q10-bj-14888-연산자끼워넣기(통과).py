fae68e54e900729acf1e191b74a4e984dee5911f
gcount85 <100mgml@gmail.com>
2022-11-14 22:05:58 +0900

    아침산책 13점 추가 + 연산자 변수명 수정

--- 3주차/Q10-bj-14888-연산자끼워넣기(통과).py
+++ 3주차/Q10-bj-14888-연산자끼워넣기(통과).py
@@ -6,23 +6,23 @@ sys.setrecursionlimit(10**9)
 
 N = int(sys.stdin.readline().strip())
 A = list(map(int, sys.stdin.readline().strip().split()))
-operator = list(map(int, sys.stdin.readline().strip().split()))  # + - X / → 2 1 1 1 
-연산자 = ['+', '-', '*', '//']
-엣지갯수 = sum(operator)
-edges = [[] for _ in range(엣지갯수)]
+operator_input = list(map(int, sys.stdin.readline().strip().split()))  # + - X / → 2 1 1 1 
+operator_ex = ['+', '-', '*', '//']
+sum_edges = sum(operator_input)
+edges = [[] for _ in range(sum_edges)]
 
 idx = 0
-for j, v in enumerate(operator):     #2, 1, 1, 1
+for j, v in enumerate(operator_input):      
     while v != 0:
-        edges[idx] = 연산자[j]
+        edges[idx] = operator_ex[j]
         v -= 1
         idx += 1
 
-연산자순열 = set(list(permutations(edges)))
+oper_permuts = set(list(permutations(edges)))
 최소 = 1000000000
 최대 = -100000000
 
-def dfs_visit(idx: int, i: tuple):        # i = ('+', '/', '*', '-', '+')
+def dfs_visit(idx: int, i: tuple):   
     global tmp, A
     if idx == N-1:
         return
@@ -50,7 +50,7 @@ def dfs_visit(idx: int, i: tuple):        # i = ('+', '/', '*', '-', '+')
                     tmp -= A[idx+1]
         dfs_visit(idx+1, i)
 
-for i in 연산자순열:
+for i in oper_permuts:
     tmp = 0
     idx = 0
     dfs_visit(idx, i)
