e94a33a24974c366366fab4df4994e87458f3ef8
100mgml@gmail.com <100mgml@gmail.com>
2022-11-15 17:13:30 +0900

    최소비용 구하기 반례 찾음

--- 3주차/Q15-bj-1916-최소비용구하기.py
+++ 3주차/Q15-bj-1916-최소비용구하기.py
@@ -5,10 +5,14 @@ import sys
 
 N = int(sys.stdin.readline())  # 도시의개수
 M = int(sys.stdin.readline())  # 버스의개수
-edges = [[] for _ in range(N+1)]
+edges = [{} for _ in range(N+1)]    
 for _ in range(M):
     src, dst, weight = map(int, sys.stdin.readline().split())
-    edges[src].append((dst, weight))  
+    if dst in edges[src].keys():  
+        if edges[src][dst] > weight:
+            edges[src][dst] = weight  
+    else:
+        edges[src][dst] = weight  
 A, B = map(int, sys.stdin.readline().split())  # 출발지점, 도착지점
 
 def bfs(s, edges):
@@ -20,7 +24,7 @@ def bfs(s, edges):
     while frontier:     # 탐색할 노드가 존재하지 않을 때까지
         nexts = []      # 다음 단계에서 탐색할 노드를 담을 리스트 
         for u in frontier:          # frontier에 있는 시작노드들에 대해
-            for v in edges[u[0]]:  #v: (2, 5)     
+            for v in edges[u[0]].items():  #v: (2, 5)     
                 if (v[0] not in level):  
                     level[v[0]] = i    # v의 레벨은 i단계
                     parent[v[0]] = u   # v의 부모 노드는 u
@@ -30,6 +34,6 @@ def bfs(s, edges):
                     weight[v[0]] = weight[u[0]] + v[1]
         frontier = nexts    # 추가 된 다음 노드들을 frontier에 올림
         i += 1
-    print(weight[B])
+    print(weight)
 
 bfs((A,0), edges)  
