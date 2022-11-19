55cb75057b209c0ac04f73c74e225bd7e73fff7a
gcount85 <100mgml@gmail.com>
2022-11-19 23:05:45 +0900

    동전 문제 입력 추가

---
+++ 4주차/Q3-bj-9084-동전.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/9084
+
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    coins = list(map(int, sys.stdin.readline().split()))
+    M = int(sys.stdin.readline())
+
+
+
+
+
+
