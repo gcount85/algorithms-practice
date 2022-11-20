9f630b883a000f88ae5c1102fbadf712970ef083
gcount85 <100mgml@gmail.com>
2022-11-21 00:14:07 +0900

    9084 동전 문제 해결

--- 4주차/Q3-bj-9084-동전.py
+++ 4주차/Q3-bj-9084-동전.py
@@ -4,34 +4,22 @@ import sys
 
 T = int(sys.stdin.readline())
 
-'''
-1
-2
-1 2
-1000
-'''
-
 for _ in range(T):
     N = int(sys.stdin.readline())
     coins = list(map(int, sys.stdin.readline().split()))
     M = int(sys.stdin.readline())
 
-fib = {}
-# 1, 2원으로 1000원을 만드는 경우의 수
-for k in range(1, M+1):
-    if k == 1:
-        f = 1
-    elif k == 2:
-        f = 2
-    else:
-        if k % 2 == 0:
-            f = fib[k-coins[0]] + 1 
-        else:
-            f = fib[k-coins[0]]
-    fib[k] = f
+    # 경우의 수를 담은 DP 테이블 
+    table = dict.fromkeys(range(1,M+1), 0)
 
-print(fib[M])
-
+    # 돈의 가치에 따라 경우의 수 계산하기
+    for c in coins:       
+        for k in table.keys():   
+            if k == c:          # 동전과 돈의 가치가 같을때, 경우의 수는 하나밖에 없음
+                table[k] += 1
+            elif k > c:         # 돈의 가치가 동전의 가치보다 클 때, 동전의 가치만큼 뺀 값의 경우의 수를 더함
+                table[k] += table[k-c] 
+    print(table[M])
 
 
 
