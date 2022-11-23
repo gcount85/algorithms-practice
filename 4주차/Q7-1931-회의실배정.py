1b5d79ba4b918fe3424e3b84900add401eff8530
gcount85 <100mgml@gmail.com>
2022-11-23 21:56:37 +0900

    1931 강의실 배정 문제 해결

--- 4주차/Q7-1931-회의실배정.py
+++ 4주차/Q7-1931-회의실배정.py
@@ -7,18 +7,16 @@ schedule = []
 
 for _ in range(N):
     s, e = map(int, sys.stdin.readline().split())
-    schedule.append((s,e,e-s))
+    schedule.append((s,e))
 
-최대개수 = 0
-schedule.sort(key=lambda x: (x[0], x[1], x[2]))
-for i in schedule:
-    reserved = [i]
-    for j in schedule:
-        if j[0] >= reserved[-1][1]:
-            reserved.append(j)
-    현재개수 = len(reserved)
-    if 현재개수 > 최대개수:
-        최대개수 = 현재개수
+schedule.sort(key=lambda x: (x[1], x[0]))
+
+last = schedule[0]
+최대개수 = 1
+for i in schedule[1:]:   # 이 범위를 그냥 schedule로 설정하면 틀림! 
+    if i[0] >= last[1]:
+        last = i
+        최대개수 += 1
 
 # print(schedule)
 print(최대개수)
