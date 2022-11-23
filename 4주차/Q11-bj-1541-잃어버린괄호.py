758517d8fb6a851bb95190eac6461f50c8df34bb
gcount85 <100mgml@gmail.com>
2022-11-23 20:24:26 +0900

    1541 잃어버린 괄호 문제 해결

--- 4주차/Q11-bj-1541-잃어버린괄호.py
+++ 4주차/Q11-bj-1541-잃어버린괄호.py
@@ -1,1 +1,24 @@
 # https://www.acmicpc.net/problem/1541
+
+import sys
+
+input = sys.stdin.readline().rstrip().split("-")
+
+# print(input)
+if len(input) == 1:
+    ans = sum(list(map(int, input[0].split("+"))))
+    del input[0]
+else:    # 뺄셈이 한개 이상 존재
+    a = sum(list(map(int, input[0].split("+"))))
+    b = sum(list(map(int, input[1].split("+"))))
+    ans = a - b
+    del input[0]
+    del input[0]
+
+while len(input) != 0:
+    a = sum(list(map(int, input[0].split("+"))))
+    ans -= a
+    del input[0]
+         
+print(ans)
+
