db0afd22cde81a8add4ac1feaf27488011b6fbf1
gcount85 <100mgml@gmail.com>
2022-12-09 01:05:19 +0900

    엔퀸 다시 푸는 중

--- 개인풀이/bj-9663-Nqueen.py
+++ 개인풀이/bj-9663-Nqueen.py
@@ -4,34 +4,33 @@
 import sys
 
 N = int(sys.stdin.readline())
-
-# 8*8, (1,1)부터 시작 
-edges = [[0] * N for _ in range(N)] 
 count = 0
-parent = {}          # 부모 노드를 지정해줌으로써 이미 방문했는지 아닌지 판별
-def dfs_visit(tup: tuple):
-    r = tup[0] # 행
-    c = tup[1] # 열
-    for i, v in enumerate(edges):
-        for j, k in enumerate(v):
-            if (i == r):
+def dfs_visit(r, c, edges):  # 0,0
+    global count
+    if r < N:
+        for col in range(N):
+            if (col in edges[:r]):
                 continue
-            if (j == c):
-                continue
-            if (i-r == j-c):
-                continue
-            else:
-                edges[i][k] = 1
-    for v in edges:      
-        if (v == 1): # 목적지 노드의 부모노드가 지정되지 않았다면(이전에 방문 안 했으면)
-            dfs_visit(v, edges) # 해당 목적지 노드를 대상으로 재귀
+            for i, e in enumerate(edges[:r]):
+                if (r-c == i-e):
+                    continue
+                else:
+                    edges[r] = col
+                    break
+            if edges[r] == -1:
+                return
+            dfs_visit(r+1, col, edges)
+    # if r == N:
+        # count += 1
+        # return
 
-
-# dfs 함수는 단절된 그래프, 강하게 연결된 그래프가 아닌 경우에 시작 노드를 바꿔 모든 그래프를 탐색하기 위함
-def dfs(N, edges):
+def dfs(N):
     for r in range(N):  # 모든 노드들에 대해 
         for c in range(N): 
-            dfs_visit((r,c))
-            # if n번째 줄까지 도달했으면 count += 1
+            edges = [-1] * N  # 8*8, (0, 0)부터 시작 
+            edges[r] = c
+            dfs_visit(r+1, c, edges) # 0,0 // 0,1 // ... 
 
+dfs(N)
 
+print(count)
