d222a638adb74dcbba553d1d3e96efe8b275f47c
gcount85 <100mgml@gmail.com>
2023-01-24 10:59:55 +0900

    Feat: 1074 Z 개인풀이 추가
    -

---
+++ 개인풀이/bj-1074-Z.py
@@ -0,0 +1,41 @@
+# https://www.acmicpc.net/problem/1074
+# 크기가 2N × 2N인 2차원 배열을 Z모양으로 탐색
+# N > 1인 경우, 배열을 크기가 2N-1 × 2N-1로 4등분 한 후에 재귀적으로 순서대로 방문
+# N이 주어졌을 때, r행 c열을 몇 번째로 방문하는지 출력
+# 정수 N, r(행), c(열)
+# 1 ≤ N ≤ 15
+# 0 ≤ r, c < 2^N
+
+
+import sys
+input = sys.stdin.readline
+
+
+def Z(N, r, c):
+    global count
+    smaller = (2**(N-1))
+    if (N == 0):
+        return count
+    else:
+        if (r < smaller) and (c < smaller):  # 제1분면
+            count += (0 * (2**(2*N-2)))
+            return Z(N-1, r, c)
+        elif (r >= smaller) and (c < smaller):  # 제2분면
+            count += (1 * (2**(2*N-2)))
+            return Z(N-1, r-smaller, c)
+        elif (r < smaller) and (c >= smaller):  # 제3분면
+            count += (2 * (2**(2*N-2)))
+            return Z(N-1, r, c-smaller)
+        elif (r >= smaller) and (c >= smaller):  # 제4분면
+            count += (3 * (2**(2*N-2)))
+            return Z(N-1, r-smaller, c-smaller)
+
+
+N, r, c = map(int, input().split())  # 2, 3, 1
+count = 0
+print(Z(N, r, c))
+
+
+# 1. 재귀 브레이크 (2^0 * 2^0 크기의 배열에 도착했을 때?)
+# 2. 1,2,3,4 분면 중 어디에 속하는지 판별 → 제일 작은 Z에 도달
+# 3. count ? (1분면 0, 2분면 +1 ... )
