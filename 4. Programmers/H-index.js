806af5b5e369e99d1bb9b91c78db7dbebd6935f2
gcount85 <100mgml@gmail.com>
2023-03-20 21:51:42 +0900

    [stash] [Programmers H-index]
    -

--- 4. Programmers/H-index.js
+++ 4. Programmers/H-index.js
@@ -1,12 +1,16 @@
 // 컨트롤 + 알트 + N 눌러서 실행!
 
 function solution(citations) {
+  const n = citations.length;
   citations.sort((a, b) => b - a);
-  // console.log(citations); // 	[ 6, 5, 3, 1, 0 ]
+  // console.log(citations); // [42, 22]
   let temp = 0;
 
-  for (let i = 0; i < citations.length; i++) {
-    if (i + 1 >= citations[i] && citations[i + 1] <= citations[i]) {
+  const minValue = Math.min(...citations);
+  console.log(minValue);
+
+  for (let i = 0; i < n; i++) {
+    if (i + 1 >= citations[i]) {
       return citations[i];
     }
   }
@@ -15,6 +19,6 @@ function solution(citations) {
 // 내림차순 정렬
 // 배열[i]보다 큰 원소가 배열[i]개 이상? 랑 같거나 큰지 확인
 // 계속 업데이트
-
-citations = [3, 0, 6, 1, 5];
+//           0    1
+citations = [42, 22];
 solution(citations);
