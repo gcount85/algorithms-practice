a0820dcd253d91cf79d918dacf2828f464d0b4bd
gcount85 <100mgml@gmail.com>
2023-03-25 10:59:48 +0900

    refactor : [BOJ 1157 단어공부]
    -

--- 1. BOJ/개인풀이/1157-단어공부.py
+++ 1. BOJ/개인풀이/1157-단어공부.py
@@ -2,14 +2,25 @@
 import sys
 from collections import Counter
 
-input = sys.stdin.readline().lower().strip()
+input_str = sys.stdin.readline().lower().strip()
 
-if (len(input) == 1):
-    print(input[0].upper())
+# if (len(input_str) == 1):
+#     print(input_str[0].upper())
+# else:
+#     char_counter = list(Counter(input_str).items())
+#     char_counter.sort(key=lambda x: (-x[1]))
+#     if (char_counter[0][1] == char_counter[1][1]):
+#         print("?")
+#     else:
+#         print(char_counter[0][0].upper())
+
+# refactored
+if (len(input_str) == 1):
+    print(input_str.upper())
 else:
-    c = list(Counter(input).items())
-    c.sort(key=lambda x: (-x[1]))
-    if (c[0][1] == c[1][1]):
+    char_counter = Counter(input_str)
+    first, second = char_counter.most_common(2)  # 가장 자주 등장한 단어 1, 2등
+    if (first[1] == second[1]):
         print("?")
     else:
-        print(c[0][0].upper())
+        print(first[0].upper())
