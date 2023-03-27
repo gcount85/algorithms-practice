b30cfdd25762bf89085d06a77f35797c3a5269e4
gcount85 <100mgml@gmail.com>
2023-03-27 14:53:56 +0900

    feat : [BOJ 10866 덱]
    -

--- 6. Practice/연습장-none과[].py
+++ 6. Practice/연습장-none과[].py
@@ -2,15 +2,15 @@ a = []
 b = None
 
 
-if a == []:
+if a == []:     # true
     print("a는 []이다")
-if a:
+if a:           # false    
     print("a는 존재한다")
-if a == None:
+if a == None:   # false   
     print("a는 []이면서 None이다")
-if b:
+if b:           # false   
     print("b는 존재한다")
-if b == []:
+if b == []:     # false   
     print("b는 none이면서 []이다")
 
     
