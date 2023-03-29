2c3ff5105eb1959aaaf31c58ed757bf9547d8b1c
gcount85 <100mgml@gmail.com>
2023-03-29 19:02:59 +0900

    refactor : [BOJ 18111 마인크래프트]
    -

--- 1. BOJ/개인풀이/18111-마인크래프트.js
+++ 1. BOJ/개인풀이/18111-마인크래프트.js
@@ -1,3 +1,4 @@
+// refactored
 const fs = require('fs');
 
 const [[N, M, B], ...map] = fs
@@ -6,30 +7,28 @@ const [[N, M, B], ...map] = fs
   .split('\n')
   .map((nums) => nums.split(' ').map(Number));
 
-// console.log(map);
-
 const min = Math.min(...map.flat());
 const max = Math.max(...map.flat());
 let count = Infinity;
 let answer;
 
-for (let i = min; i < max + 1; i++) {
+for (let stdHeight = min; stdHeight < max + 1; stdHeight++) {
   let tempBlock = B;
   let tempCount = 0;
   for (const row of map) {
     for (const height of row) {
-      const gap = height - i;
+      const gap = height - stdHeight;
       if (gap === 0) {
         continue;
-      } else if (gap > 0) {
+      }
+      if (gap > 0) {
         // 땅 높이가 높아서 블록을 빼야할 때
-        tempBlock += gap;
         tempCount += 2 * gap;
       } else {
         // 땅이 낮아서 블록을 쌓아야 할 때
-        tempBlock += gap;
         tempCount -= gap;
       }
+      tempBlock += gap;
     }
   }
   if (tempBlock < 0) {
@@ -37,7 +36,7 @@ for (let i = min; i < max + 1; i++) {
   }
   if (tempCount <= count) {
     count = tempCount;
-    answer = i;
+    answer = stdHeight;
   }
 }
 
