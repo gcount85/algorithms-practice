4bf7c353f7363e22bb132b57f1f9a34f7d11aa0c
gcount85 <100mgml@gmail.com>
2023-03-31 00:03:07 +0900

    fix : [BOJ 10250 ACM 호텔]
    -

--- 10250-ACM호텔.py
+++ 10250-ACM호텔.py
@@ -6,12 +6,12 @@ import sys
 T = int(sys.stdin.readline())
 for _ in range(T):
     H, W, N = map(int, sys.stdin.readline().split())
-    층 = N % H   # 10 % 6
+    층 = N % H   #
     호 = N // H  # 10 // 6
     if (층 != 0):
         호 += 1
     if (층 == 0):
-        층 = 6
+        층 = H
     if 호 < 10:
         호 = "0" + str(호)
     print(str(층)+str(호))
