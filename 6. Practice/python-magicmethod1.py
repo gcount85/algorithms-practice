8d2396881ad16bb9fe028635c49fd20eb77ec37c
gcount85 <100mgml@gmail.com>
2023-04-13 23:48:11 +0900

    feat : Add python magic method exmple 2
    -

--- 6. Practice/python-magicmethod1.py
+++ 6. Practice/python-magicmethod1.py
@@ -18,7 +18,7 @@ a = MyClassA(5)
 b = MyClassB(5)
 
 print(a.str())  # Output: MyClassA: 5
-print(str(a)) # Output: <__main__.MyClassA object at 0x7f8e291d76d0> 
+print(str(a))  # Output: <__main__.MyClassA object at 0x7f8e291d76d0>
 # 빌트인 str() 메소드를 사용하지 못함!
 
 print(b.__str__())  # Output: MyClassB: 5
