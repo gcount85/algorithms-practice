1c87a6d475104c77ed33e0ba7d1ccad8ab77220f
gcount85 <100mgml@gmail.com>
2023-04-23 22:00:19 +0900

    refactor : [BOJ 1254 팰린드롬 만들기]
    -

--- 1. BOJ/개인풀이/1254-팰린드롬만들기.py
+++ 1. BOJ/개인풀이/1254-팰린드롬만들기.py
@@ -1,25 +1,25 @@
+# https://www.acmicpc.net/problem/1254
+# refactored
+
 import sys
 
 input = sys.stdin.readline
-
 string = input().strip()
-string_copy = string
+
 n = len(string)
 temp_str = ""
-for i in range(n):
-    isPalindrome = 1
-    s, e = 0, len(string_copy)-1
-    while s < e:
-        if string_copy[s] != string_copy[e]:
-            isPalindrome = 0
-            break
-        s += 1
-        e -= 1
+string_copy = string
 
-    if isPalindrome == 0:
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+
+for i in range(n):
+    if not is_palindrome(string_copy):
         temp_str = string[i] + temp_str
         string_copy = string + temp_str
     else:
         break
 
-print(len(string_copy))
+print(n + i)
