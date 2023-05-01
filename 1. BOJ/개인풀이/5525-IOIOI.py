c709f4ea2f4ca8e6d22cfeb139a648639de9c4bd
gcount85 <100mgml@gmail.com>
2023-05-01 15:11:12 +0900

    fix : [BOJ 5525 IOIOI]
    -

--- 1. BOJ/개인풀이/5525-IOIOI.py
+++ 1. BOJ/개인풀이/5525-IOIOI.py
@@ -1,50 +1,59 @@
-# 50점
-
+# 100점 (O(N))
 import sys
 
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
 s = sys.stdin.readline().strip()
 
-def check_pattern(l, h):
-    tmp_l, tmp_h = l, h
-    while tmp_l <= tmp_h:
-        ioi = 'I'
-        if (tmp_l - l) % 2 == 1:
-            ioi = 'O'
-        if s[tmp_l] != s[tmp_h]:
-            return False
-        if s[tmp_l] != ioi or s[tmp_h] != ioi:
-            return False
-        tmp_l += 1
-        tmp_h -= 1
-    return True
+def check_pattern(s, n):
+    count = 0
+    consecutive_ioi = 0
+    i = 0
+
+    while i < len(s) - 2:
+        if s[i:i+3] == "IOI":
+            consecutive_ioi += 1
+            if consecutive_ioi == n:
+                count += 1
+                consecutive_ioi -= 1 # 중복 패턴 카운트 
+            i += 2
+        else:
+            consecutive_ioi = 0
+            i += 1
+
+    return count
+
+print(check_pattern(s, n))
+
+
+# def check_pattern(l, h):
+#     tmp_l, tmp_h = l, h
+#     while tmp_l <= tmp_h:
+#         ioi = 'I'
+#         if (tmp_l - l) % 2 == 1:
+#             ioi = 'O'
+#         if s[tmp_l] != s[tmp_h]:
+#             return False
+#         if s[tmp_l] != ioi or s[tmp_h] != ioi:
+#             return False
+#         tmp_l += 1
+#         tmp_h -= 1
+#     return True
     
 
-l, h = 0, 2 * n
-count = 0
-while h < m:
-    if check_pattern(l, h):
-        # print(s[l:h+1])
-        count += 1
-        l += 2
-        h += 2
-    else:
-        l += 1
-        h += 1
+# l, h = 0, 2 * n
+# count = 0
+# while h < m:
+#     if check_pattern(l, h):
+#         # print(s[l:h+1])
+#         count += 1
+#         l += 2
+#         h += 2
+#     else:
+#         l += 1
+#         h += 1
     
-print(count)
-
-''' 
-- IOI 형태로 있는지 확인하는 함수
-    - 양쪽 끝 문자가 I가 아니면 pass 
-    - 양쪽에서 두 번째 문자들이 O가 아니면 pass
-    - 양쪽에서 세번째 문자열의 인덱스가 
-- m부터 길이 3까지만 확인 (p1은 길이 3부터이므로)
-- 찾고자 하는 값은 길이가 m일때의 dp 값  
-- 길이가 n일때의 dp값은 ? 
-
-'''
+# print(count)
 
 
 
