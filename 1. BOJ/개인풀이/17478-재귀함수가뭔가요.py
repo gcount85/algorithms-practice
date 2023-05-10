c343feab477768d73236b1a78d2e1b5aec7da626
gcount85 <100mgml@gmail.com>
2023-05-10 19:24:38 +0900

    feat : [BOJ 17478 재귀함수가 뭔가요?]
    -

---
+++ 1. BOJ/개인풀이/17478-재귀함수가뭔가요.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/17478
+
+from sys import stdin
+
+def solution(n, rcurs, count):
+    add = "____" * count
+    if count == n:
+        for r in rcurs[0], rcurs[5], rcurs[4]:
+            print(add + r)
+        return
+    for r in rcurs[:4]:
+        print(add + r)
+    solution(n, rcurs, count + 1)
+    print(add + rcurs[4])
+
+intro = '어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.'
+rcurs1 = '"재귀함수가 뭔가요?"'
+rcurs2 = '"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.'
+rcurs3 = '마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.'
+rcurs4 = '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."'
+rcurs5 = '라고 답변하였지.'
+rcurs6 = '"재귀함수는 자기 자신을 호출하는 함수라네"'
+
+n = int(stdin.readline())
+count = 0
+print(intro)
+solution(n, [rcurs1, rcurs2, rcurs3, rcurs4, rcurs5, rcurs6], count)
