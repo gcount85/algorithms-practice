2e58a5f5b735d72b8e3c9633023abfd3d5f4b9f5
gcount85 <100mgml@gmail.com>
2025-01-25 13:22:30 +0900

    feat : [BOJ 2752 세수정렬]

---
+++ 09. 정렬/1. BOJ/2752-세수정렬.js
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/2752
+
+const fs = require("fs");
+
+const inputs = fs
+  .readFileSync("./dev/stdin", "utf-8")
+  .trim()
+  .split(" ")
+  .map(Number);
+  
+inputs.sort((a, b) => a - b);
+console.log(inputs.join(" "));
+
